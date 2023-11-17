6c360a5f962ae71cec347315e6d5d32b36a48040
mrMoraess <matheusrodriguesde347@gmail.com>
2023-11-17 19:31:48 -0300

    Adição de novas funcionalidades

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,20 +5,56 @@ const el = {
     input_find: document.querySelector('#find-task'),
     btnAdd: document.querySelector('#btn-add'),
     btnFind: document.querySelector('#btn-find'),
-    tasksContainer: document.querySelector('.tasks-container')
+    tasksContainer: document.querySelector('.tasks-container'),
+    input_edit: document.querySelector('#edit-task'),
+}
+
+const divContent = (div) => {
+    let textContent = ''
+
+    div.childNodes.forEach((node) => {
+        if (node.nodeType === node.TEXT_NODE) {
+            textContent += node.textContent
+        }
+    });
+
+    return textContent.trim()
 }
 
 const addTask = (task) => {
-    let divTask = document.createElement('div')
-    divTask.classList.add('task-style')
-    divTask.innerText = task
+    if (task.length > 5) {
+        let divTask = document.createElement('div')
+        let divContent = document.createElement('div')
+        divContent.classList.add('task-content')
+        divContent.innerText = task
+        divTask.classList.add('task-style')
+        divTask.appendChild(divContent)
 
-    let btnOptions = document.createElement('button')
-    btnOptions.classList.add('btn-task-option')
-    btnOptions.textContent = '...'
+        let btnContainer = document.createElement('div')
+        btnContainer.classList.add('btn-container')
+    
+        let btnDelete = document.createElement('button')
+        btnDelete.classList.add('btn-task-delete')
+        btnDelete.classList.add('btn-task-style')
+        btnDelete.innerText = 'D'
 
-    divTask.appendChild(btnOptions)
-    el.tasksContainer.appendChild(divTask)
+        let btnEdit = document.createElement('button')
+        btnEdit.classList.add('btn-task-edit')
+        btnEdit.classList.add('btn-task-style')
+        btnEdit.innerText = "E"
+
+        let btnConcluir = document.createElement('button')
+        btnConcluir.classList.add('btn-task-concluir')
+        btnConcluir.classList.add('btn-task-style')
+        btnConcluir.innerText = 'C'
+
+        btnContainer.appendChild(btnDelete)
+        btnContainer.appendChild(btnConcluir)
+        btnContainer.appendChild(btnEdit)
+
+        divTask.appendChild(btnContainer)
+        el.tasksContainer.appendChild(divTask)
+    }
 }
 
 el.btnAdd.addEventListener('click', (e) => {
@@ -27,17 +63,92 @@ el.btnAdd.addEventListener('click', (e) => {
 })
 
 const findTask = (task) => {
-    const nodeList = document.querySelectorAll('.task-style')
-    let findedTask = []
-    nodeList.forEach((e) => {
-        if (e.textContent === task) {
-            findedTask.push(e)
+    const nodeList = document.querySelectorAll('.task-content')
+    let index = false
+    console.log(nodeList)
+    nodeList.forEach((e, i) => {
+        console.log(e.textContent)
+        if (e.textContent.toLowerCase() == task.toLowerCase()) {
+            e.parentNode.style.border = "2px solid red"
+            setTimeout(function () {
+                e.parentNode.style.border = ''
+            }, 2500)
+            index = i
         } 
     })
 
-    console.log(findedTask)
+    if (index) {
+        let pai = nodeList[index].parentNode.parentNode
+        pai.removeChild(nodeList[index].parentNode)
+        pai.insertBefore(nodeList[index].parentNode, pai.firstChild)
+    }
+    // depois de encontrar a(s) tasks que estão sendo procuradas vou coloca-las
+    // no topo da lista e destaca-las com uma borda laranja
 }
 
 el.btnFind.addEventListener('click', (e) => {
     findTask(el.input_find.value)
+})
+
+const deleteTask = (task) => {
+    let pai = task.parentNode.parentNode // pai do pai
+    pai.parentNode.removeChild(pai) // pai do pai do pai, remove o pai do pai
+}
+
+const editTask = (task) => {
+    document.querySelector('.edit-task-container').style.display = 'flex'
+    let pai = task.parentNode.parentNode
+    el.input_edit.value = ''
+    el.input_edit.value = pai.querySelector('.task-content').textContent
+    console.log(pai.querySelector('.task-content').textContent)
+
+    return new Promise((resolve, reject) => {
+        let editedTask = ''
+        document.querySelector('#btn-edit').addEventListener('click', (e) => {
+            console.log('oi')
+            if (e.target.className === 'form-button' && el.input_edit.value.trim() !== '') {
+                editedTask = el.input_edit.value.trim()
+                el.input_edit.value = ''
+                resolve(editedTask)
+
+                setTimeout(() => {
+                    document.querySelector('.edit-task-container').style.display = 'none'
+                }, 500)
+            } else {
+                reject('Edição inválida.')
+            }
+        })
+    })
+}
+
+const concluirTask = (btnConcluir) => {
+    let pai = btnConcluir.parentNode.parentNode
+    pai.style.border = '2px solid rgb(0, 255, 0)'
+
+    setTimeout(() => { 
+        pai.style.border = '' 
+        deleteTask(btnConcluir)
+    }, 1500)
+
+}
+
+el.tasksContainer.addEventListener('click', async (e) => {
+    console.log(e.target.className)
+    if (e.target.className === 'btn-task-delete btn-task-style') {
+        console.log('delete')
+        deleteTask(e.target)
+    } else if (e.target.className === 'btn-task-edit btn-task-style') {
+        console.log('edit')
+        try {
+            const editedTask = await editTask(e.target)
+            let pai = e.target.parentNode.parentNode
+            console.log(pai)
+            pai.querySelector('.task-content').textContent = editedTask
+        } catch(e) {
+            console.log(e)
+        }
+    } else if (e.target.className === 'btn-task-concluir btn-task-style') {
+        console.log('concluir')
+        concluirTask(e.target)
+    }
 })
